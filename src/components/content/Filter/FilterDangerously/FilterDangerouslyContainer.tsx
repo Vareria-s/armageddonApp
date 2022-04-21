d196ab3396a1c40f606f81a8828ab94149619aa8
Lera Abramova <67120537+Vareria-s@users.noreply.github.com>
2022-04-21 12:14:10 +0500

    TS

--- src/components/content/Filter/FilterDangerously/FilterDangerouslyContainer.tsx
+++ src/components/content/Filter/FilterDangerously/FilterDangerouslyContainer.tsx
@@ -3,9 +3,10 @@ import {useContext} from "react";
 import {MyContext} from "../../../../App";
 import FilterDangerously from "./FilterDangerously";
 
+
+
 export const FilterDangerouslyContainer = () => {
-    // @ts-ignore
     const {state, dispatch} = useContext(MyContext);
     return <FilterDangerously  onlyDangerous={state.onlyDangerous}
-                              changeOnlyDangerous={()=>dispatch({type: 'CHANGE_ONLY_DANGEROUS',})}/>
+                              changeOnlyDangerous={()=>dispatch({type: 'CHANGE_ONLY_DANGEROUS'})}/>
 }
